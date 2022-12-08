47f12ed1cd4f26cb921c89479f514574f849b0ca
ridvan <ridvandikmen1997@gmail.com>
2022-12-08 13:39:30 +0300

    _05_Hello

---
+++ src/_05_Hello.java
@@ -0,0 +1,15 @@
+public class _05_Hello {
+    public static void main(String[] args){
+        /*
+        *Bir alt satıra inmek için \n kullanıyoruz
+         */
+        System.out.println("Hello");
+        System.out.print("Bir alt satıra inmek için\n");
+        System.out.print("Bir alt satıra inmek için ");
+
+
+
+
+
+    }
+}
