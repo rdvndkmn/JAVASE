706f63cc332cae89848d799f0f2a370a82cbfdc1
rdvndkmn <ridvandikmen1997@gmail.com>
2023-01-03 20:54:07 +0300

    a

--- src/_17_ArrayDeclarations.java
+++ src/_17_ArrayDeclarations.java
@@ -4,5 +4,6 @@ public class _17_ArrayDeclarations {
         double [][]dizi = new double[2][3];
         //Bu tarz farklı şekillerde de tanımlama yapılabilir.
         // double [][]dizi1 = [2][3]new double; Bu şekilde hata verir.
+        //
     }
 }
