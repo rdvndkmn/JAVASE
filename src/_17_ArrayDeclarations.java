2ec8951c72e4ea03ca2a55496e7d8d2c54d3aab9
ridvan <ridvandikmen1997@gmail.com>
2023-01-03 20:53:24 +0300

    03.01.2023

---
+++ src/_17_ArrayDeclarations.java
@@ -0,0 +1,8 @@
+public class _17_ArrayDeclarations {
+    public static void main(String[] args){
+
+        double [][]dizi = new double[2][3];
+        //Bu tarz farklı şekillerde de tanımlama yapılabilir.
+        // double [][]dizi1 = [2][3]new double; Bu şekilde hata verir.
+    }
+}
